87ae608b66ce24bec3d80ecd1a36e2d6e380749e
kitamreik <kitdamreik@gmail.com>
2024-07-31 03:55:04 -0600

    rollback and reset, moved files into pantry folder once refactored

--- pages/pantry-log.js
+++ pages/pantry-log.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+//import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-//import Link from 'next/link';
+import Link from 'next/link';
 
 export default function Pantry() {
   return (
---
+++ pages/pantry/content.js
@@ -0,0 +1,13 @@
+// pages/content.js
+import React from 'react';
+import PantryPost from './content.mdx';
+
+const ContentPage = () => {
+  return (
+    <div>
+      <PantryPost />
+    </div>
+  );
+};
+
+export default ContentPage;
